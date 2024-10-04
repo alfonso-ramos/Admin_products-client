5b7c83afcbe904b1b4ff0de3dcfb25714f6674af
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-10-04 12:36:21 -0700

    [ADD] Agregado el form con captura de datos y validacion para campos vacios

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,6 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, Form, useActionData, ActionFunctionArgs } from "react-router-dom";
+import ErrorMessage from "../components/ErrorMessage";
+
+export async function action({request} : ActionFunctionArgs) {
+  //Recupera datos del formulario
+  const data = Object.fromEntries(await request.formData())
+  // console.log(data)
+  let error = ''
+  
+  if(Object.values(data).includes('')){
+    error = 'Todos los campos son obligatorios'
+  }
+
+  if(error.length){
+    return error
+  }
+
+  return {}
+}
 
 export default function NewProduct() {
+
+   const error = useActionData() as string
+   console.log(error)
+   
   return (
     <>
       <div className="flex justify-between">
@@ -12,6 +34,39 @@ export default function NewProduct() {
           Volver a Productos
         </Link>
       </div>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+
+      <Form className="mt-10" method="POST" action="">
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="name">
+            Nombre Producto:
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Nombre del Producto"
+            name="name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="price">
+            Precio:
+          </label>
+          <input
+            id="price"
+            type="number"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Precio Producto. ej. 200, 300"
+            name="price"
+          />
+        </div>
+        <input
+          type="submit"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+          value="Registrar Producto"
+        />
+      </Form>
     </>
   );
 }
